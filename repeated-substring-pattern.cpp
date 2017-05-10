15c48fc0f61ac5684d2d117e9a6d604e4c6e4a6d
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-05-10 11:50:47 +0530

    Repeated Substring Pattern solution

---
+++ repeated-substring-pattern.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int i, l, n = s.length(), flag = 0;
+        for (l = 1; l <= n/2; l++)
+        {
+            string str = s.substr(0, l);
+            flag = 0;
+            for (i = 0; i < n; i += l)
+            {
+                if (i+l-1 >= n)
+                {
+                    flag = 1;
+                    break;
+                }
+                string test = s.substr(i, l);
+                if (test != str)
+                {
+                    flag = 1;
+                    break;
+                }
+
+            }
+            if (!flag)
+                return true;
+        }
+        return false;
+    }
+};
