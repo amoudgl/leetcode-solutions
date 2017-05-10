9422a5cf1b55e3be652ff21dc150d94907871642
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-05-10 08:04:13 +0530

    Island Perimeter solution

---
+++ island-perimeter.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int i, j, ans = 0, n = grid.size(), m = grid[0].size();
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    if ((j-1 >= 0 && grid[i][j-1] == 0) || j-1 < 0)
+                        ans += 1;
+                    if ((j+1 < m && grid[i][j+1] == 0) || j+1 == m)
+                        ans += 1;
+                    if ((i-1 >= 0 && grid[i-1][j] == 0) || i-1 < 0)
+                        ans += 1;
+                    if ((i+1 < n && grid[i+1][j] == 0) || i+1 == n)
+                        ans += 1;           
+                }
+            }
+        }
+        return ans;
+    }
+};
