7f425698956abe1a0236bab91fdbab764db7b5a9
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-05-10 08:13:43 +0530

    Valid Parentheses solution

---
+++ valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int i;
+        for (i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+            else if (!st.empty() && ((s[i] == ')' && st.top() == '(') || (s[i] == ']' && st.top() == '[') || (s[i] == '}' && st.top() == '{')))
+                st.pop();
+            else
+                return false;
+        }
+        if (st.empty())
+            return true;
+        return false;
+    }
+};
