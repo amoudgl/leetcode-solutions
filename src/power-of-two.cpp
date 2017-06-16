f07c663e53fe791ff8b90f45ddb5f27cefd282b9
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-06-16 09:29:22 +0530

    Math, Bit Manipulation

---
+++ src/power-of-two.cpp
@@ -0,0 +1,8 @@
+// Author: Abhinav Moudgil [ https://leetcode.com/amoudgl/ ]
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0 ? true : false;
+    }
+};
